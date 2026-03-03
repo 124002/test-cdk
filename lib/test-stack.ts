33632682ca9b56ffc32bcefcfb15c339feadf50e
124002-asai <asai_yuga@comture.com>
2026-03-04 08:52:42 +0900

    second time push

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,11 +1,12 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // ここに何も書かなくてもデプロイは成功する
+    // S3バケットを1個作成
+    new s3.Bucket(this, 'TestBucket');
   }
 }
-``
